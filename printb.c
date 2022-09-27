a42fbefdb270496909feefc96b7f3fa95e0912e4
LE Dylan <dylan.le@ecole-89.com>
2022-09-27 16:28:55 +0200

    printb.c

---
+++ printb.c
@@ -0,0 +1,23 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     2
+ * created on:  2022-09-27 - 15:39 +0200
+ * 1st author:  dylan.le - dylan.le
+ * description: Exo 2
+ */
+
+
+#include <unistd.h>
+
+void stu_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int main(void)
+{
+    stu_putchar('b');
+    stu_putchar('\n');
+    return(0);
+
+}
