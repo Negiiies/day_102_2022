da02520fd1a4e7e5f11456bb58c439ddb62a12b5
LE Dylan <dylan.le@ecole-89.com>
2022-09-27 16:07:11 +0200

    ret189.c

---
+++ ret189.c
@@ -0,0 +1,13 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     tata
+ * created on:  2022-09-27 - 15:21 +0200
+ * 1st author:  dylan.le - dylan.le
+ * description: toto
+ */
+
+int main(void)
+{
+    return(189);
+
+}
