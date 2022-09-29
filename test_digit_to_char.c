88ea40abda6b107507da77cc2902e4c17ba63e49
LE Dylan <dylan.le@ecole-89.com>
2022-09-29 16:53:18 +0200

    test_digit_to_char.c

---
+++ test_digit_to_char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+char stu_digit_to_char(int nb);
+int main(void)
+
+{
+char tmp;
+tmp = stu_digit_to_char(5);
+if (tmp == '5') {
+    printf("%d\n", tmp);
+} else {
+    printf("%d\n", tmp);
+}
+}
