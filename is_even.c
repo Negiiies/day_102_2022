beec803a5af8ee33810e5e800e54e86e29dae0c0
LE Dylan <dylan.le@ecole-89.com>
2022-09-29 11:28:21 +0200

    is_odd

---
+++ is_even.c
@@ -0,0 +1,13 @@
+ /*
+ * E89 Pedagogical & Technical Lab
+ * project:     pair
+ * created on:  2022-09-27 - 16:48 +0200
+ * 1st author:  dylan.le - dylan.le
+ * description: nombre pair
+ */
+
+
+int is_even(int nb2)
+{
+    return ((nb2+1)%2);
+}
---
+++ is_odd.c
@@ -0,0 +1,13 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     impairs
+ * created on:  2022-09-29 - 09:08 +0200
+ * 1st author:  dylan.le - dylan.le
+ * description: nb impairs
+ */
+
+int is_odd(int nb )
+{
+    return (nb%2);
+}
+
