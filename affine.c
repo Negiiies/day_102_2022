e9e60b46677b3309be21be524ce93079dc20335d
LE Dylan <dylan.le@ecole-89.com>
2022-09-30 16:51:01 +0200

    affine

---
+++ affine.c
@@ -0,0 +1,14 @@
+
+int stu_affine_i(int a, int b, int x)
+{
+    int nb;
+    nb = a*b+x;
+    return (nb);
+}
+
+float stu_affine_f(float a, float b, float x)
+{
+    float res;
+    res = a*b+x;
+    return (res);
+}
---
+++ test_affine.c
@@ -0,0 +1,7 @@
+int stu_affine_i(int a, int b, int x);
+float stu_affine_f(float a, float b, float x);
+int main (void)
+{
+    return (stu_affine_i(8, 5, 4));
+    return (stu_affine_f(8, 5, 4));
+}
