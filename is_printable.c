d9d58247e4c41002e9fc987590807be25382a330
LE Dylan <dylan.le@ecole-89.com>
2022-09-30 15:35:10 +0200

    is_printable

---
+++ is_printable.c
@@ -0,0 +1,9 @@
+
+char stu_is_printable(int nb)
+{
+    if ('!'<= nb && nb <= '~' ) {
+        return (1);
+    } else {
+        return (0);
+    }
+}
---
+++ test_is_printable.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+char stu_is_printable(int nb);
+int main(void)
+{
+    return  stu_is_printable('g');
+}
