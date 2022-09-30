cc9d60981c2e28c74552787d89cb5fdc465e4096
LE Dylan <dylan.le@ecole-89.com>
2022-09-30 16:30:20 +0200

    abs

---
+++ abs.c
@@ -0,0 +1,8 @@
+int stu_abs(int nb)
+{
+    if (nb>0){
+        return (nb);
+    }else{
+        return (nb-nb-nb);
+    }
+}
---
+++ test_abs.c
@@ -0,0 +1,5 @@
+int stu_abs(int nb);
+int main(void)
+{
+    return stu_abs(5);
+}
