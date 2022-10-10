401f99afeedf0455982c68975b5d28e8ce612e76
LE Dylan <dylan.le@ecole-89.com>
2022-10-10 09:56:22 +0200

    update

--- digit_to_char.c
+++ digit_to_char.c
@@ -1,7 +1,5 @@
 char stu_digit_to_char(int nb)
 {
-    if(nb<=9){
-        return ('0'+nb);
-    }
-    return(0);
+    nb = nb + '0';
+    return (nb);
 }
--- test_digit_to_char.c
+++ test_digit_to_char.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 char stu_digit_to_char(int nb);
 int main(void)
-
 {
-char tmp;
-tmp = stu_digit_to_char(5);
-if (tmp == '6') {
-    printf("%d\n", tmp);
-    puts("OK");
-} else {
-    printf("%d\n", tmp);
-    puts("not OK");
-}
+    char tmp;
+    tmp = stu_digit_to_char(5);
+    if (tmp == '6') {
+        printf("%d\n", tmp);
+        puts("OK");
+    } else {
+        printf("%d\n", tmp);
+        puts("not OK");
+    }
 }
