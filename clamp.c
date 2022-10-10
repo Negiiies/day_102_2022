61b320854f49440a5e711561dd8b85e0b223883b
LE Dylan <dylan.le@ecole-89.com>
2022-10-10 09:57:25 +0200

    update

--- clamp.c
+++ clamp.c
@@ -2,10 +2,10 @@ int stu_clamp(int l, int h, int n)
 {
     if (n < l){
         return (l);
-         }
+    }
     if (n > h){
         return (h);
     } else{
-     return (n);
-        }
+        return (n);
+    }
 }
